bf83b481671c6949b8657993bc2cccfae71ccad4
Melanie Day <melamday@protonmail.com>
2026-01-19 16:37:44 -0500

    Upgrade to fix link issue and add text for pub

--- go.mod
+++ go.mod
@@ -7,6 +7,6 @@ require github.com/maxence-charriere/go-app/v10 v10.1.8
 require (
 	github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/ladyofmazes/linkbook v0.2.2 // indirect
+	github.com/ladyofmazes/linkbook v0.2.4 // indirect
 	github.com/maxence-charriere/app v1.3.6 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -38,7 +38,6 @@ func (h *figure1) OnMount(ctx app.Context) {
 			h.figurepage.IpageVisits[val] = visits
 		})
 	}
-
 }
 
 func (h *figure1) Render() app.UI {
@@ -63,14 +62,47 @@ type figure2 struct {
 	figurepage *lbook.FigurePage
 }
 
-func (h *figure2) Render() app.UI {
-	var curPage = lbook.NewFigurePage()
+func (h *figure2) OnMount(ctx app.Context) {
+	h.figurepage = lbook.NewFigurePage()
+	h.figurepage.Page("sealed-with-a-kiss-drinks")
 
-	return curPage.
+	h.figurepage.Icaptions = []string{"Click Below to Begin",
+		"You go to the local pub",
+		"After several drinks you don't feel well",
+		"Somehow you forgot you are allergic to alcohol",
+		"You died (but luckily were resurrected by a cleric because this is a magical world)"}
+	h.figurepage.Ilinks = []string{"", "", "", "", "/"}
+	// Load the stored value
+	for i, val := range h.figurepage.Ipage {
+		ctx.Dispatch(func(ctx app.Context) {
+			var value int
+			ctx.SessionStorage().Get(h.figurepage.Ipage[i], &value)
+
+			h.figurepage.IpageScore[val] = value
+
+			var visits int
+			ctx.SessionStorage().Get(h.figurepage.Ipage[i]+"Visits", &visits)
+
+			h.figurepage.IpageVisits[val] = visits
+		})
+	}
+}
+
+func (h *figure2) Render() app.UI {
+	if h.figurepage == nil {
+		h.figurepage = lbook.NewFigurePage()
+	}
+	if len(h.figurepage.Icaptions) == 0 {
+		h.figurepage.Icaptions = []string{""}
+	}
+	if len(h.figurepage.Ilinks) == 0 {
+		h.figurepage.Ilinks = []string{""}
+	}
+	return h.figurepage.
 		Name("sealed-with-a-kiss-drinks").
 		Figure(
 			"/web/20251129_173150.png",
-		).Audio("/web/Forsaken.wav")
+		).Caption(h.figurepage.Icaptions...).Audio("/web/Forsaken.wav").Links(h.figurepage.Ilinks...)
 }
 
 type figure3 struct {
@@ -79,9 +111,16 @@ type figure3 struct {
 }
 
 func (h *figure3) Render() app.UI {
-	var curPage = lbook.NewFigurePage()
-
-	return curPage.
+	if h.figurepage == nil {
+		h.figurepage = lbook.NewFigurePage()
+	}
+	if len(h.figurepage.Icaptions) == 0 {
+		h.figurepage.Icaptions = []string{""}
+	}
+	if len(h.figurepage.Ilinks) == 0 {
+		h.figurepage.Ilinks = []string{""}
+	}
+	return h.figurepage.
 		Name("sealed-with-a-kiss-bakery").
 		Figure(
 			"/web/20251208_121710.png",
@@ -94,13 +133,64 @@ type figure4 struct {
 }
 
 func (h *figure4) Render() app.UI {
-	var curPage = lbook.NewFigurePage()
-
-	return curPage.
+	if h.figurepage == nil {
+		h.figurepage = lbook.NewFigurePage()
+	}
+	if len(h.figurepage.Icaptions) == 0 {
+		h.figurepage.Icaptions = []string{""}
+	}
+	if len(h.figurepage.Ilinks) == 0 {
+		h.figurepage.Ilinks = []string{""}
+	}
+	return h.figurepage.
 		Name("sealed-with-a-kiss-room").
 		Figure(
 			"/web/20251201_174639.png",
 		).Audio("/web/QuietChat.wav")
+}
+
+type figure5 struct {
+	app.Compo
+	figurepage *lbook.FigurePage
+}
+
+func (h *figure5) Render() app.UI {
+	if h.figurepage == nil {
+		h.figurepage = lbook.NewFigurePage()
+	}
+	if len(h.figurepage.Icaptions) == 0 {
+		h.figurepage.Icaptions = []string{""}
+	}
+	if len(h.figurepage.Ilinks) == 0 {
+		h.figurepage.Ilinks = []string{""}
+	}
+	return h.figurepage.
+		Name("sealed-with-a-kiss-door").
+		Figure(
+			"/web/20251230_171946.png",
+		).Audio("/web/Trepidation.wav")
+}
+
+type figure6 struct {
+	app.Compo
+	figurepage *lbook.FigurePage
+}
+
+func (h *figure6) Render() app.UI {
+	if h.figurepage == nil {
+		h.figurepage = lbook.NewFigurePage()
+	}
+	if len(h.figurepage.Icaptions) == 0 {
+		h.figurepage.Icaptions = []string{""}
+	}
+	if len(h.figurepage.Ilinks) == 0 {
+		h.figurepage.Ilinks = []string{""}
+	}
+	return h.figurepage.
+		Name("sealed-with-a-kiss-kiss").
+		Figure(
+			"/web/20260104_155132.png",
+		).Audio("/web/CheerfulSunshine.wav")
 }
 
 func main() {
@@ -108,6 +198,8 @@ func main() {
 	app.Route("/sealed-with-a-kiss-drinks", func() app.Composer { return &figure2{} })
 	app.Route("/sealed-with-a-kiss-bakery", func() app.Composer { return &figure3{} })
 	app.Route("/sealed-with-a-kiss-room", func() app.Composer { return &figure4{} })
+	app.Route("/sealed-with-a-kiss-door", func() app.Composer { return &figure5{} })
+	app.Route("/sealed-with-a-kiss-kiss", func() app.Composer { return &figure6{} })
 
 	app.RunWhenOnBrowser()
 
